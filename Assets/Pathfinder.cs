fdb1cfd89d0f1373008ab827420992f886b8b3e9
GanzaMos <moskalik.ganza@gmail.com>
2020-06-28 19:47:08 +0300

    9 Explore Neighbours function in PathFinding script

--- Assets/Pathfinder.cs
+++ Assets/Pathfinder.cs
@@ -6,32 +6,55 @@ using UnityEngine;
 public class Pathfinder : MonoBehaviour
 {
 
-    Dictionary<Vector2Int, Waypoint> path = new Dictionary<Vector2Int, Waypoint>();
+    Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
     [SerializeField] Waypoint startWaypoint, endWaypoint;
+
+    Vector2Int[] directions = {
+        Vector2Int.up,
+        Vector2Int.right,
+        Vector2Int.left,
+        Vector2Int.down
+    };
 
     void Start()
     {
         DictionaryOfBlocks();
-        startWaypoint.SetTopColor(Color.blue);
+        ExploreNeighbours();
+        startWaypoint.SetTopColor(Color.green);
         endWaypoint.SetTopColor(Color.red);
     }
 
-
+    void ExploreNeighbours ()
+    {
+        foreach(Vector2Int direction in directions)
+        {
+            Vector2Int explorationCoordinates = startWaypoint.GetGridPos() + direction;
+            try
+            {
+                grid[explorationCoordinates].SetTopColor(Color.blue);
+            }
+            catch
+            {
+                //do nothing
+            }
+            
+        }
+    }
 
     private void DictionaryOfBlocks()
     {
         foreach (Waypoint waypoint in FindObjectsOfType<Waypoint>())
         {
-            if (path.ContainsKey(waypoint.GetGridPos()))
+            if (grid.ContainsKey(waypoint.GetGridPos()))
             {
                 print("Duplicate Block in position " + waypoint.GetGridPos());
             }
             else
             {
-                path.Add(waypoint.GetGridPos(), waypoint);
+                grid.Add(waypoint.GetGridPos(), waypoint);
             }
         }
-        print("Number of Blocks - " + path.Count);
+        print("Number of Blocks - " + grid.Count);
     }
 
     // Update is called once per frame
